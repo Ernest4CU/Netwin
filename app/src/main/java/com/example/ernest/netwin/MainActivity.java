cdd47d959d45651a8f5d3627d84b0c5ce2922dde
Ernest4CU <Ernest_xin@outlook.com>
2016-09-06 14:36:15 +0800

    基本完成功能（除了串口的协议）

--- app/src/main/java/com/example/ernest/netwin/MainActivity.java
+++ app/src/main/java/com/example/ernest/netwin/MainActivity.java
@@ -1,14 +1,304 @@
 package com.example.ernest.netwin;
 
+import android.app.Activity;
+import android.icu.util.Calendar;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+import com.dalong.marqueeview.MarqueeView;
+import com.dwin.dwinapi.SerialPort;
 
+import org.apache.http.util.EncodingUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    public Handler adsTextSetHandler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 1:
+                    AdsText="广告设置成功";
+                    //adsTextView.setText(AdsText);
+                    try {
+                        AdsText=readFileSdcardFile("/mnt/sdcard1/Netwin/advertising.txt");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    adsTextView.setText(AdsText);
+                    break;
+                default:break;
+            }
+        }
+    };
+    String[] adsVidwoList;
+    String AdsText;
+    ImageView arrowFlag;
+    TextView curFloor;
+    TextView curdate;
+    TextView dayOfWeek;
+    MarqueeView adsTextView;
+    SerialPort serialPort;
+    VideoView adsShow;
+    int intCurVideo=0;//记录当前播放的视频索引
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        initView();
+        AdsText = "广告测试 广告测试 广告测试 广告测试 广告测试 广告测试 广告测试 广告测试 " +
+                "广告测试 广告测试 广告测试 广告测试 广告测试 广告测试 广告测试 广告测试 广告测试 " +
+                "广告测试 广告测试 广告测试 广告测试 广告测试 广告测试 广告测试 广告测试 广告测试 ";
+        adsTextView.setText(AdsText);
+        adsTextView.startScroll();
+
+        closeBar();//关闭导航栏
+//        serialPort = new SerialPort("S0", 9600, 8, 1, 110);
+//        new ReceiveThread().start();//测试串口接收数据
+        openSerialPort();
+        //开启AP
+        WifiHostBiz wifiHostBiz = new WifiHostBiz(this);
+        if (!wifiHostBiz.isWifiApEnabled()) {
+            wifiHostBiz.setWifiApEnabled(true);
+        }
+        //更新广告
+        try {
+            AdsText=readFileSdcardFile("/mnt/sdcard1/Netwin/advertising.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        adsTextView.setText(AdsText);
+        new TimeThread().start();//开启日期更新进程，每秒更新一次
+        new downThread().start();//开启网络服务器来监听上传任务
+        setVideoAdd();//添加视频播放目录
+        adsShow.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                if(++intCurVideo==3)
+                    intCurVideo=0;
+                videoDisplay(adsVidwoList[intCurVideo]);
+            }
+        });
+        videoDisplay(adsVidwoList[0]);
     }
+
+    private void initView()
+    {
+        arrowFlag = (ImageView) findViewById(R.id.arrowFlag);
+        curFloor = (TextView) findViewById(R.id.curFloor);
+        curdate = (TextView) findViewById(R.id.curdate);
+        dayOfWeek = (TextView) findViewById(R.id.dayOfWeek);
+        adsTextView = (MarqueeView) findViewById(R.id.AdsTextView);
+        adsShow = (VideoView) findViewById(R.id.adsShow);
+    }
+
+    private void openSerialPort(){
+        serialPort = new SerialPort("S0", 9600, 8, 1,110);
+        new ReceiveThread().start();
+    }
+
+    //读SD中的文件
+    public String readFileSdcardFile(String fileName) throws IOException {
+        String res = "";
+        try {
+            FileInputStream fin = new FileInputStream(fileName);
+
+            int length = fin.available();
+
+            byte[] buffer = new byte[length];
+            fin.read(buffer);
+
+            res = EncodingUtils.getString(buffer, "gb2312");
+
+            fin.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    class downThread extends Thread{
+        @Override
+        public void run() {
+            //服务器中信息
+            try {
+                ServerSocket serverSocket = new ServerSocket(8888);
+                Socket socket = null;
+                int count=0;
+                System.out.println("***服务器即将启动，等待客户端的连接***");
+                while (true) {
+                    socket = serverSocket.accept();
+                    DownloadThread downloadThread = new DownloadThread(socket,"/mnt/sdcard1/Netwin/Video",adsTextSetHandler);
+                    downloadThread.start();
+                    count++;//用来记录客户量
+                    InetAddress address = socket.getInetAddress();
+                    System.out.println("客户端IP = " + address.getHostAddress());
+                    System.out.println("客户量 = " + count);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setVideoAdd(){
+        adsVidwoList = new String[3];
+        adsVidwoList[0]="/mnt/sdcard1/Netwin/Video/NetwinVideo0.mp4";
+        adsVidwoList[1]="/mnt/sdcard1/Netwin/Video/NetwinVideo1.mp4";
+        adsVidwoList[2]="/mnt/sdcard1/Netwin/Video/NetwinVideo2.mp4";
+    }
+
+    private void videoDisplay(String str){
+//        Uri videoUri = Uri.parse("/mnt/sdcard1/bell2.mp4");
+        Uri videoUri = Uri.parse(str);
+        adsShow.setVideoURI(videoUri);
+        adsShow.start();
+    }
+
+
+
+    /**
+     * 接收数据线程
+     */
+    class ReceiveThread extends Thread {
+        public void run() {
+            while (serialPort.isOpen) {
+                String type = "ASCII".trim();
+                String data = serialPort.receiveData(type);
+                if (data != null) {
+                    Message msg = new Message();
+                    msg.what = 1;
+                    msg.obj = data;
+                    System.out.println(data + "<<<<<<<<==========data");
+                    myHandler.sendMessage(msg);
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private android.os.Handler myHandler = new android.os.Handler() {
+        public void handleMessage(android.os.Message msg) {
+            switch (msg.what) {
+                case 1:
+                    Date date = new Date();
+                    String test = (String) msg.obj;
+                    char[] demo = test.toCharArray();
+                    if(demo.length>2){
+                        curFloor.setText(new String(demo,0,2));
+                        switch (demo[2]){
+                            case 'U':arrowFlag.setImageResource(R.drawable.up);break;
+                            case 'D':arrowFlag.setImageResource(R.drawable.down);break;
+                            default:break;
+                        }
+//                        curFloor.setText(demo[0]+demo[1]+"");
+//                        if(demo[1]>0){
+//                            arrowFlag.setImageResource(R.drawable.up);
+//                        }else {
+//                            arrowFlag.setImageResource(R.drawable.down);
+//                        }
+                    }
+//                    if(demo[0]==0x33){
+//                        textShow.setText((CharSequence) msg.obj);
+//                    }else{
+//                        textShow.setText("数据无效");
+//                    }
+
+                    break;
+                default:
+                    break;
+            }
+        };
+    };
+
+    private void closeBar(){
+        try{
+            //需要root 权限
+            Build.VERSION_CODES vc = new Build.VERSION_CODES();
+            Build.VERSION vr = new Build.VERSION();
+            String ProcID = "79";
+
+            if(vr.SDK_INT >= vc.ICE_CREAM_SANDWICH){
+                ProcID = "42"; //ICS AND NEWER
+            }
+
+            //需要root 权限
+            Process proc = Runtime.getRuntime().exec(new String[]{"su","-c","service call activity "+ ProcID +" s16 com.android.systemui"}); //WAS 79
+            proc.waitFor();
+
+        }catch(Exception ex){
+            Toast.makeText(getApplicationContext(), ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void showBar(){
+        try{
+            Process proc = Runtime.getRuntime().exec(new String[]{
+                    "am","startservice","-n","com.android.systemui/.SystemUIService"});
+            proc.waitFor();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class TimeThread extends Thread {
+        @Override
+        public void run() {
+            do {
+                try {
+                    Thread.sleep(1000);
+                    Message msg = new Message();
+                    msg.what = 1;  //消息(一个整型值)
+                    mHandler1.sendMessage(msg);// 每隔1秒发送一个msg给mHandler
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } while (true);
+        }
+    }
+
+    //在主线程里面处理消息并更新UI界面
+    private android.os.Handler mHandler1 = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            //super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    long time=System.currentTimeMillis();
+                    Date date=new Date(time);
+                    SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd");
+                    curdate.setText(format.format(date)); //更新时间
+                    format=new SimpleDateFormat("EEEE");
+                    dayOfWeek.setText(format.format(date));//更新星期
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
 }
